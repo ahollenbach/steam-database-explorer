af63c65326d3ce2cba6bea33c069f78cfb043da6
Andrew DeVoe <tonbo_guntal@comcast.net>
2013-04-16 22:53:53 -0400

    Further work on recursion
    More work.  Code is pretty ugly but I believe it is appropriately
    pulling in friends and linking friendships.  Throws a lot of exceptions
    (tries to add things multiple times).  Needs to be optimized.  Also,
    there is one special case that seems to have issues, but it seemed like
    Steam itself was handling it in an odd fashion.

--- eclipse/src/steam/dbexplorer/Main.java
+++ eclipse/src/steam/dbexplorer/Main.java
@@ -13,9 +13,12 @@ package steam.dbexplorer;
 
 import java.awt.print.Printable;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
 import com.github.koraktor.steamcondenser.steam.community.GameAchievement;
@@ -26,53 +29,55 @@ import com.github.koraktor.steamcondenser.steam.community.WebApi;
 
 public class Main {
 	
-	private final static int BRANCH = 3; //The produced friendship tree has a radius of BRANCH-1;
+	private final static int BRANCH = 2; //The produced friendship tree has a radius of BRANCH-1;
 	//Thus a BRANCH of 1 gets exactly one person (The center).
 	//  2 gets the center and the centers friends
 	//	3 gets the center, their friends, and their friends of friends
 	
+	private final static int VISIBLE = 3; //community visiblity state constants
+	private final static int PRIVATE = 1;
+
+	public static class Pair {
+		long steamId1;
+		long steamId2;
+		
+		public Pair (long l1, long l2) {
+			steamId1 = l1;
+			steamId2 = l2;
+		}
+	}
 	
-	public static HashMap<Long,Long> populatePlayers(int recursive, Statement stmt, Long longId) {
+	public static Set<Pair> populatePlayers(int recursive, Statement stmt, Long longId) {
 		if (recursive == BRANCH) {
-			return new HashMap<Long, Long>();
+			return new HashSet<Pair>();
 		}
 		else {
 			SteamId id;
-			List<SteamId> friends;
-			HashMap<Long, Long> friendsToAdd = new HashMap<Long, Long> ();
+			List<SteamId> friends = new ArrayList<SteamId>();
+			Set<Pair> friendsToAdd = new HashSet<Pair>();
 			try {
 				//basic steam API code, TEST
 				stmt.execute("select count(steamId) as playerExists from player where steamId = "+longId+";");
 				ResultSet rs = stmt.getResultSet();
 				rs.next();
 				if (rs.getInt("PlayerExists") > 0) {
-					return new HashMap<Long, Long> (); //We already called populatePlayer on this person
+					return new HashSet<Pair>(); //We already called populatePlayer on this person
 				}
 				id = SteamId.create(longId, true);
 				stmt.executeUpdate("insert into player values ("+id.getSteamId64()+", '"+id.getNickname()+"', '"+id.getCustomUrl()+"', null, null, null);");
-				//System.out.println(id.getNickname());
-				//System.out.println(id.getMemberSince());
 				try {
-					friends = id.getFriends();
-					//System.out.println("Size:"+friends.size());
-					//GameStats stats = id.getGameStats("tf2");
-					//List<GameAchievement> achievements = stats.getAchievements();
+					//if (id.getVisibilityState() == VISIBLE) {
+						friends = id.getFriends();
+					//}
 					for (SteamId temp:friends) {
 						try {
-							//System.out.println (temp.getSteamId64());
-							//SteamId f = SteamId.create(temp.getSteamId64(), true) ;
-							//System.out.println("insert into player values ("+f.getSteamId64()+", '"+f.getNickname()+"', '"+f.getCustomUrl()+"', null, null, null);");
-							//stmt.executeUpdate("insert into player values ("+f.getSteamId64()+", '"+f.getNickname()+"', '"+f.getCustomUrl()+"', null, null, null);");
+							//if (((!friendsToAdd.containsKey(id.getSteamId64()) && !friendsToAdd.containsValue(temp.getSteamId64())) && 
+							//		(!friendsToAdd.containsValue(id.getSteamId64()) && !friendsToAdd.containsKey(temp.getSteamId64()))) ) {
+									friendsToAdd.add(new Pair(id.getSteamId64(), temp.getSteamId64()));
+							//	}
 							if ((recursive+1) != BRANCH) {
-								friendsToAdd.putAll(populatePlayers(recursive+1, stmt, temp.getSteamId64()));					
+								friendsToAdd.addAll((populatePlayers(recursive+1, stmt, temp.getSteamId64())));					
 							}
-							
-							//System.out.println("insert into friend values ("+id.getSteamId64()+", "+f.getSteamId64()+");");
-							if ((!(friendsToAdd.containsKey(id.getSteamId64()) && friendsToAdd.containsValue(temp.getSteamId64())) || 
-								!(friendsToAdd.containsValue(id.getSteamId64()) && friendsToAdd.containsKey(temp.getSteamId64()))) ) {
-								friendsToAdd.put(id.getSteamId64(), temp.getSteamId64());
-							}
-							//stmt.executeUpdate("insert into friend values ("+id.getSteamId64()+", "+temp.getSteamId64()+");");
 						} catch (Exception e) {
 							e.printStackTrace();
 						}
@@ -80,29 +85,26 @@ public class Main {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
 			if (recursive == 0) {
-				for (Iterator<Long> it = friendsToAdd.keySet().iterator(); it.hasNext();) {
-					Long steamId1 = it.next();
-					Long steamId2 = friendsToAdd.get(steamId1);
+				for (Iterator<Pair> it = friendsToAdd.iterator(); it.hasNext();) {
+					Pair p = it.next();
 					try {
-						stmt.executeUpdate("insert into friend values ("+steamId1+", "+steamId2+");");
+						stmt.executeUpdate("insert into friend values ("+p.steamId1+", "+p.steamId2+");");
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
 				}
-				return new HashMap<Long,Long>();
+				return new HashSet<Pair>();
 			}
 			else {
 				return friendsToAdd;
 			}
 		}
 	}
-
+	
 	/**
 	 * @param args
 	 */
@@ -122,7 +124,7 @@ public class Main {
 			//ResultSet rs = stmt.getResultSet();
 			//rs.next();
 			//Long n = rs.getLong("steamId");
-			populatePlayers(0,stmt,76561197988128323L);
+			populatePlayers(0,stmt,76561197988083973L);
 			//76561197988083973  -  Tonbo
 			//76561197988128323  -  WispingWinds
 			//76561198018660341  -  DROCK
