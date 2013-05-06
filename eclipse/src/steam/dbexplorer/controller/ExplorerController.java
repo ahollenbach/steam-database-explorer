1099a76b21abfc773ff755d8b17de7b85598b63e
Andrew Hollenbach <anh7216@rit.edu>
2013-05-06 00:24:37 -0400

    Bugfixing: Equivalent lookups work now
    Fixed Bug where used == instead of =

--- eclipse/src/steam/dbexplorer/controller/ExplorerController.java
+++ eclipse/src/steam/dbexplorer/controller/ExplorerController.java
@@ -42,7 +42,7 @@ public class ExplorerController {
 
 	public static final String[] opEquivs = {"<",
 											 "<=",
-											 "==",
+											 "=",
 											 "<>",
 											 ">=",
 											 ">"};
